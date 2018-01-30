2f509b34b1e6a927aad11fe14f14b63d3f49e51b
student <student@GBTC441J02LX.montgomerycollege.edu>
2018-01-30 13:08:59 -0500

    Initial commit

---
+++ src/main/java/com/springboot_01/demo/HelloWorldThymeleafApplication.java
@@ -0,0 +1,12 @@
+package com.springboot_01.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldThymeleafApplication.class, args);
+	}
+}
